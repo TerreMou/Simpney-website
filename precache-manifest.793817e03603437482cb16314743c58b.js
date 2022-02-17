e86e63565b8c2cf6833c72feb0b4efc5fde57a5f
terremou <terrence_mou@foxmail.com>
2022-02-17 18:52:58 +0800

    deploy

---
+++ precache-manifest.793817e03603437482cb16314743c58b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d96aaa9542c1c76d79e",
+    "url": "/Simpney-website/css/app.a8d0e2b9.css"
+  },
+  {
+    "revision": "6995bb5dfd71b8e5c99066db47e173cf",
+    "url": "/Simpney-website/index.html"
+  },
+  {
+    "revision": "2d96aaa9542c1c76d79e",
+    "url": "/Simpney-website/js/app.2ca9dcf6.js"
+  },
+  {
+    "revision": "7e2ae6be2260920f8287",
+    "url": "/Simpney-website/js/chunk-vendors.8e0d49bc.js"
+  },
+  {
+    "revision": "da403051cc2f80acdd6c33270f100de2",
+    "url": "/Simpney-website/manifest.json"
+  },
+  {
+    "revision": "32ea31510609fdc569df479f90187843",
+    "url": "/Simpney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Simpney-website/robots.txt"
+  }
+]);
